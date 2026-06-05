369b679aed92cdc27b19d4a4f5c373fc8426c5d2
londer <148044380+lbraten@users.noreply.github.com>
2026-06-05 19:16:09 +0200

    bug fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import { fileURLToPath } from "node:url";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  vite: {
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+        "@cv": fileURLToPath(new URL("./cv.json", import.meta.url)),
+      },
+    },
+  },
 });
